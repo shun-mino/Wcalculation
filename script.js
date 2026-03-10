5bf70b008b57279f603b932f738059a4eec70791
shun-mino <149372941+shun-mino@users.noreply.github.com>
2026-03-10 11:36:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,46 @@
 function createTable(title, list, V, L) {
-    let html = `<div class="section"><h3>${title}</h3>`;
-    html += `<table>
-        <tr>
-            <th>Ω/m</th>
-            <th>t抵抗値Ω</th>
-            <th>電力(W)</th>
-            <th>電流（A）</th>
-            <th>W/m</th>
-        </tr>`;
 
-    list.forEach(r => {
-        const R = r * L;
-        const P = V * V / R;
-        comst I = V / R;
-        const Wm = P / L;
+let html = `<div class="section"><h3>${title}</h3>`;
 
-        let redClass = "";
+html += `<table>
+<tr>
+<th>Ω/m</th>
+<th>抵抗値Ω</th>
+<th>電力(W)</th>
+<th>電流(A)</th>
+<th>W/m</th>
+</tr>`;
 
-        // 
-        if (title === "TF" && Wm >= 35) redClass = "red";
-        if (title === "GH" && Wm >= 100) redClass = "red";
+list.forEach(r => {
 
-        html += `
-        <tr>
-            <td>${r}</td>
-            <td>${R.toFixed(1)}</td>
-            <td>${P.toFixed(1)}</td>
-            <td>${I.toFixed(1)}</td>
-            <td class="${redClass}">${Wm.toFixed(1)}</td>
-        </tr>`;
-    });
+const R = r * L;
+const P = V * V / R;
+const I = V / R;
+const Wm = P / L;
 
-    html += `</table></div>`;
-    return html;
+let redClass = "";
+
+if (title === "TF") {
+if (Wm >= 50) redClass = "red";
+}
+
+if (title === "GH") {
+if (Wm >= 100) redClass = "red";
+}
+
+html += `
+<tr>
+<td>${r}</td>
+<td>${R.toFixed(1)}</td>
+<td>${P.toFixed(1)}</td>
+<td>${I.toFixed(2)}</td>
+<td class="${redClass}">${Wm.toFixed(1)}</td>
+</tr>
+`;
+
+});
+
+html += `</table></div>`;
+return html;
+
 }
